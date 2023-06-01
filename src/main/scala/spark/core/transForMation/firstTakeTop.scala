3217a6f267b175c012fe89d1d03e098a6a3413e3
myismamingyang <126977818+myismamingyang@users.noreply.github.com>
2023-06-01 23:59:19 +0800

    firstTakeTop.scala

---
+++ src/main/scala/spark/core/transForMation/firstTakeTop.scala
@@ -0,0 +1,18 @@
+package spark.core.transForMation
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object firstTakeTop {
+  def main(args: Array[String]): Unit = {
+    val conf: SparkConf = new SparkConf().setAppName("spark").setMaster("local[*]")
+    val sc: SparkContext = new SparkContext(conf)
+    sc.setLogLevel("WARN")
+
+    val rdd: RDD[Int] = sc.parallelize(List(3, 6, 1, 2, 4, 5))
+    println(rdd.first())
+    rdd.take(3).foreach(print)
+    println()
+    rdd.top(3).foreach(print)
+  }
+}
