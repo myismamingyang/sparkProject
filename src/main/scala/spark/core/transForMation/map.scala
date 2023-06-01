d1c02c2ebc85e21fa8f26c13261875cb69a4846d
myismamingyang <126977818+myismamingyang@users.noreply.github.com>
2023-06-01 16:05:03 +0800

    data

--- src/main/scala/spark/core/transForMation/map.scala
+++ src/main/scala/spark/core/transForMation/map.scala
@@ -1,6 +1,7 @@
 package spark.core.transForMation
 
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.catalyst.dsl.expressions.intToLiteral
 import org.apache.spark.{SparkConf, SparkContext}
 
 object map {
@@ -8,8 +9,9 @@ object map {
     val conf: SparkConf = new SparkConf().setAppName("spark").setMaster("local[*]")
     val sc: SparkContext = new SparkContext(conf)
     sc.setLogLevel("WARN")
-    val rdd1: RDD[Int] = sc.parallelize(List(5, 6, 4, 7, 3, 8, 2, 9, 1, 10))
-    val values: RDD[Int] = rdd1.map(_ * 3).collect
-    values.foreach(println)
+    val rdd1: RDD[Int] = sc.parallelize(List(5, 6, 4))
+    rdd1.map(_ * 3).collect.foreach(println)
+    println("-------1-------")
+    rdd1.map(_*3).foreach(println)
   }
 }
