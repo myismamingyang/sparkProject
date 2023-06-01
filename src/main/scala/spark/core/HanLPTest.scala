23b5d07adcdd08aa356490b56b209a65d3879900
myismamingyang <126977818+myismamingyang@users.noreply.github.com>
2023-06-01 14:17:10 +0800

    1
    1

---
+++ src/main/scala/spark/core/HanLPTest.scala
@@ -0,0 +1,27 @@
+package spark.core
+
+import java.util
+
+import com.hankcs.hanlp.HanLP
+import com.hankcs.hanlp.seg.common.Term
+
+object HanLPTest {
+  def main(args: Array[String]): Unit = {
+    val words = "[HanLP入门案例]"
+    println("1: " + words)
+    val terms: util.List[Term] = HanLP.segment(words)
+    val terms1 = HanLP.segment(words)
+    println(terms1)
+    println("2: " + terms) //直接打印java的list:[[/w, HanLP/nx, 入门/vn, 案例/n, ]/w]
+    import scala.collection.JavaConverters._
+    println("3: " + terms.asScala.map(_.word)) //转为scala的list:ArrayBuffer([, HanLP, 入门, 案例, ])
+    val cleanWords1: String = words.replaceAll("\\[|\\]", "") //将"["或"]"替换为空"" //"HanLP入门案例"
+    println("4: " + cleanWords1) //HanLP入门案例
+    println("5: " + HanLP.segment(cleanWords1).asScala.map(_.word)) //ArrayBuffer(HanLP, 入门, 案例)
+
+    val log = """00:00:00 2982199073774412    [360安全卫士]   8 3 download.it.com.cn/softweb/software/firewall/antivirus/20067/17938.html"""
+    val cleanWords2 = log.split("\\s+")(2) //[360安全卫士]
+      .replaceAll("\\[|\\]", "") //360安全卫士
+    println("6: " + HanLP.segment(cleanWords2).asScala.map(_.word)) //ArrayBuffer(360, 安全卫士)
+  }
+}
