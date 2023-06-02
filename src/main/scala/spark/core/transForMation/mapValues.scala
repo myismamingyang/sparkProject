0cf08d5fb6b6945f8d8fa4f57bae9b4d371545ac
myismamingyang <126977818+myismamingyang@users.noreply.github.com>
2023-06-02 14:40:58 +0800

    mapValues.scala

---
+++ src/main/scala/spark/core/transForMation/mapValues.scala
@@ -0,0 +1,14 @@
+package spark.core.transForMation
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object mapValues {
+  def main(args: Array[String]): Unit = {
+    val conf: SparkConf = new SparkConf().setAppName("spark").setMaster("local[*]")
+    val sc: SparkContext = new SparkContext(conf)
+    sc.setLogLevel("WARN")
+    val values: RDD[(Int, Int)] = sc.parallelize(List((1, 10), (2, 20), (3, 30)))
+    values.mapValues(_ * 2).collect().foreach(println)
+  }
+}
