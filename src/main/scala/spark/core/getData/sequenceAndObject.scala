35c7f85762a88a940d74982082d642ab09a2fb58
myismamingyang <126977818+myismamingyang@users.noreply.github.com>
2023-06-07 15:35:04 +0800

    1

---
+++ src/main/scala/spark/core/getData/sequenceAndObject.scala
@@ -0,0 +1,34 @@
+package spark.core.getData
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * @Author: Mingyang Ma
+ * @Date: 2023/6/7 14:39
+ * @Version: 1.0
+ * @Function:
+ */
+object sequenceAndObject {
+  def main(args: Array[String]): Unit = {
+    val sparkConf: SparkConf = new SparkConf().setAppName("wc").setMaster("local[*]")
+    val sc: SparkContext = new SparkContext(sparkConf)
+    sc.setLogLevel("WARN")
+
+    val result: RDD[(String, Int)] = sc.textFile("data/input/words.txt")
+      .flatMap(_.split(" "))
+      .map((_, 1)) //_表示每一个单词
+      .reduceByKey(_ + _)
+
+    result.coalesce(1).saveAsSequenceFile("data/output/sequence") //保存为序列化文件
+    result.coalesce(1).saveAsObjectFile("data/output/object") //保存为对象文件
+
+    val rdd1: RDD[(String, Int)] = sc.sequenceFile("data/output/sequence") //读取sequenceFile
+    val rdd2: RDD[(String, Int)] = sc.objectFile("data/output/object") //读取objectFile
+
+    rdd1.foreach(println)
+    rdd2.foreach(println)
+
+    sc.stop()
+  }
+}
